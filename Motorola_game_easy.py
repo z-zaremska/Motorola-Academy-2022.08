0e76ea0b4bce05a937b9b69b48c76d82c7466f7e
Zuza Zaremska <110463932+z-zaremska@users.noreply.github.com>
2022-08-10 21:15:15 +0200

    Update Motorola_game_easy.py
    - code clean up
    - add description to functions
    - use while loop to continue with next rounds
    - set condition of winning and loosing the game
    - fixed updating the actual round and guess chances counting

--- Motorola_game_easy.py
+++ Motorola_game_easy.py
@@ -8,7 +8,6 @@ with open('Words.txt') as words_file:
     words_for_game = words_file.read()
 
 list_of_words = words_for_game.split('\n')
-words_in_game = []
 
 
 #Zdefiniowanie funkcji wykorzystywanych w grze
@@ -20,80 +19,106 @@ def draw_words_for_game(list_of_words):
         words_in_game.append(random_word)
 
 
-def use_chance(guess_chances):
-    """Countdown guess chances"""
-    guess_chances -= 1
-    return guess_chances
-
-def next_round(guess_chances, round_0, revealed_items):
+def update_board(guess_chances, round_0):
     """Clears previous game board and shows new one
     with updated game status."""
-
     import os
     os.system('cls')
 
-    next_round_board = f"""\nLevel: easy
-    Guess chances: {guess_chances}
-    You already revealed: {revealed_items}
-    —-----------------------------------
-    _ 1 2 3 4
-    A {round_0['a1']} {round_0['a2']} {round_0['a3']} {round_0['a4']}
-    B {round_0['b1']} {round_0['b2']} {round_0['b3']} {round_0['b4']}
-    —-----------------------------------
-    """
-    print(next_round_board)
+    updated_board = f"""\nLevel: easy
+Guess chances: {guess_chances}
+Round: {10-guess_chances}
+—-----------------------------------
+_ 1 2 3 4
+A {round_0['a1'][0]} {round_0['a2'][0]} {round_0['a3'][0]} {round_0['a4'][0]}
+B {round_0['b1'][0]} {round_0['b2'][0]} {round_0['b3'][0]} {round_0['b4'][0]}
+—-----------------------------------
+"""
+    print(updated_board)
+
+def set_random_word():
+    """Choses random word and removes it from words in game list."""
+    random_word = random.choice(words_in_game)
+    words_in_game.remove(random_word)
+    return random_word
+
+
+def uncover(user_choice):
+    """Changes 'X' into 'word' from words_in_game."""
+    return round_0[user_choice][1]
+
+
+def cover(user_choice):
+    """Changes 'word' from words_in_game into 'X'."""
+    return round_0[user_choice][2]
+
+
+def pick_first_word(user_choice_A):
+    """Uncovers first word chosen by player."""
+    round_0[user_choice_A][0] = uncover(user_choice_A)
+
+
+def pick_maching_word(guess_chances, user_choice_A, user_choice_B):
+    if round_0[user_choice_B][1] == round_0[user_choice_A][1]:
+        round_0[user_choice_B][0] = uncover(user_choice_B)
+        update_board(guess_chances, round_0)
+        print(f'\nCongratulations!\nYou have uncoverd a pair of "{round_0[user_choice_B][1]}"')
+        uncovered_pairs.append({round_0[user_choice_B][1]})
+        go_on = input('\nIf you want to continue: press any.\nIf you want to quit press "q".\nYour answear: ')
+        if go_on.lower() == 'n':
+            print('\nGame over.')
+    else:
+        round_0[user_choice_B][0] = uncover(user_choice_B)
+        update_board(guess_chances, round_0)
+        print(f"""\nYou've uncovered word '{round_0[user_choice_B][1]}' ({user_choice_B}).
+Unfortunately it's not the same as the one you've uncoverd last time: '{round_0[user_choice_A][1]}' ({user_choice_A}).""")
+        go_on = input('\nIf you want to continue: press any.\nIf you want to quit press "q".\nYour answear: ')
+        if go_on.lower() == 'n':
+            print('\nGame over.')
+        round_0[user_choice_A][0] = cover(user_choice_A)
+        round_0[user_choice_B][0] = cover(user_choice_B)
 
     
 
-#Gra - poziom EASY
-
-#Runda 0 - Początek gry
+#Game setup (Round 0)
+#Choose random 4 words for the game.
+uncovered_pairs = []
+words_in_game = []
 draw_words_for_game(list_of_words)
-print(words_in_game)
-
+#Start chances
 guess_chances = 10
+#Prepare game board
 round_0 = {
-    'a1': 'X',
-    'a2': 'X',
-    'a3': 'X',
-    'a4': 'X',
-    'b1': 'X',
-    'b2': 'X',
-    'b3': 'X',
-    'b4': 'X',
+    'a1': ['X', set_random_word(), 'X'],
+    'a2': ['X', set_random_word(), 'X'],
+    'a3': ['X', set_random_word(), 'X'],
+    'a4': ['X', set_random_word(), 'X'],
+    'b1': ['X', set_random_word(), 'X'],
+    'b2': ['X', set_random_word(), 'X'],
+    'b3': ['X', set_random_word(), 'X'],
+    'b4': ['X', set_random_word(), 'X'],
 }
 
-print(f"""\nLevel: easy
-    Guess chances: {guess_chances}
-    —-----------------------------------
-    _ 1 2 3 4
-    A {round_0['a1']} {round_0['a2']} {round_0['a3']} {round_0['a4']}
-    B {round_0['b1']} {round_0['b2']} {round_0['b3']} {round_0['b4']}
-    —-----------------------------------
-    """)
-
-revealed_items = []
-
-#Runda 1/10
-first_user_choice = input("""Which "X" you want to reveal first?
+#Gameplay (Rounds 1-10 -> 5x Round A + Round B)
+while True:
+    #Round A
+    update_board(guess_chances, round_0)   
+    user_choice_A = input("""Which "X" you want to uncover?
 Your answear: """)
-round_0[first_user_choice] = random.choice(words_in_game)
-revealed_items.append(first_user_choice)
-guess_chances = use_chance(guess_chances)
+    guess_chances -= 1
+    pick_first_word(user_choice_A)
+    #Round B
+    update_board(guess_chances, round_0)
+    user_choice_B = input("""Which "X" you want to uncover?
+Your answear: """)
+    guess_chances -= 1
+    pick_maching_word(guess_chances, user_choice_A, user_choice_B)
 
+    if len(uncovered_pairs) == 4:
+        print('\nYOU WON!')
+        break
 
-#Rundy 2 - 9
-have_chances = True
-while have_chances:
-    next_round(guess_chances, round_0, revealed_items)
-    next_user_choice = input("""Which "X" you want to reveal next?
-    Your answear: """)
-    round_0[next_user_choice] = random.choice(words_in_game)
-    revealed_items.append(next_user_choice)
-    guess_chances = use_chance(guess_chances)
-    if guess_chances == 0:
-        next_round(guess_chances, round_0, revealed_items)
-        have_chances = False
-
-print('[GAME OVER] You have lost.')
-
+    elif guess_chances == 0:
+        print('\nGAME OVER')
+        print(f'You have uncovered: {uncovered_pairs}')
+        break
